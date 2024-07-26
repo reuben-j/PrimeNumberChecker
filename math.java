e78273c31cfed1e881dcd9323769d7f4b73cdb27
reuben-j <reubenjarvela@gmail.com>
2024-07-26 07:09:36 +0100

    JavaPrimerChecker

---
+++ math.java
@@ -0,0 +1,20 @@
+public class math {
+
+    static Boolean isPrime(int num) {
+        if(num <= 1) {
+            return false;
+        } else{
+            for(int i = 2; i < num-1; i++) {
+                if(num % i == 0) {
+                    System.out.println(i);
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(20));
+    }
+}
